6a161dee5ff2a211b3dc2a44d65b5c5ea8daf6ec
Josh Latto <josh.latto@gmail.com>
2016-11-27 12:39:20 -0800

    Create placeholder request handlers

---
+++ requestHandlers.js
@@ -0,0 +1,16 @@
+// Define placeholder functions for request handling
+
+function start() {
+	console.log("Request handler 'start' was called");
+}
+
+function upload() {
+	console.log("Request handler 'upload' was called");
+}
+
+// Export them
+
+exports.start  = start;
+exports.upload = upload;
+
+
